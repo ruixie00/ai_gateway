c3685dd632fb4ebe142e1b31e9f29dc4ee83d5ed
ruixie00 <ruixie00@gmail.com>
2026-02-22 13:25:40 +0800

    Change server port from 3000 to 8080

--- ai_gateway/index.js
+++ ai_gateway/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // 【关键的第三行】优先使用服务器分配的端口，如果没有就用 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // 允许接收 JSON 格式的数据
 app.use(express.json());
@@ -26,3 +26,4 @@ app.post('/api/chat', (req, res) => {
 app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 网关已在端口 ${port} 启动`);
 });
+
